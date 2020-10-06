991a519773019959e24ea2a7fc8fc02a131e20e3
Ice K <68528319+alive2020@users.noreply.github.com>
2020-10-06 18:04:50 +0900

    Create js.js

---
+++ js.js
@@ -0,0 +1,7 @@
+alert("okoko");
+var hhh = document.getElementById("center");
+hhh.addEventListener("click", function(){
+    this.classList.toggle("text-center");
+    this.classList.toggle("container");
+}
+)
