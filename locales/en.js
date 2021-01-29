51e38cf01672f5aa7d4dc6330c0dd85651e8edd9
Ice Calee <68528319+alive2020@users.noreply.github.com>
2021-01-29 20:31:27 +0900

    added key-value for eng pages
    key-value blocks by page

--- locales/en.js
+++ locales/en.js
@@ -4,16 +4,160 @@ export default {
     Product:"Products",
     Recipes:"Recipes",
     Contact:"Contact us",
-    title:"Korean products",
-    titlep:"Shop for mayonnaise, bread crumbs, ramen, various sauces, drinks and more at Upkaite!",
-    titlebuttontext:"See more",
+    
+     History: "History",
+    Location: "Location",
+    Other: "Other",
+
+
+
+    title: "Korean products",
+    titlep: "Shop for mayonnaise, bread crumbs, ramen, various sauces, drinks and more at Upkaite!",
+    titlebuttontext: "See more",
     intro: "Upkite has over 30 years of experience, It is the most famous Korean grocery trading company in Russia and CIS countries!",
-    whyustitle:"Why Choose Us?",
+    whyustitle: "Why Choose Us?",
     whyus: "Uplait officially works with many Korean co nglomerates and SMEs.We get low prices from our factories thanks to large orders and talented people with years of trading experience. We deliver our products fast and accurately.",
     whyusmore: "Read More",
+    brand: "UPKAIT",
     companytitle: "Сompanies we cooperate with",
-    category:"Category",
-    weight:"Capacity",
-    company:"Manifacture",
-    expiration:"Expiration date"
-}
+
+
+
+
+    brand_low: "Upkait",
+    about_head: "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Expedita nesciunt utdelectus, omnis repellendus quae sint vitae culpa temporibus dicta.",
+    about_sub_title: "About us",
+    about_sub_text1: "Trading company UPKITE carries out wholesale supplies of food
+    across the territory of the Far East, Siberia, Kazakhstan, Uzbekistan.< br >
+        We offer a wide range of food products - including mayonnaise,
+            ketchups, wasabi and soy sauce, noodles, rusks and pastries, drinks and coffee, milk powder and cream(certified products).< br >
+                <br>
+                    Our company has been working on the market for more than 25 years with constant success and stability.<br>
+                        Distributor relations with leading manufacturers of South Korea
+                        allow you to offer a wide range of products and a flexible system of discounts
+                        clients of our company. The goods undergo strict quality control and accompanying documentation.<br>
+                            <br>
+                                Our company is focused on customer needs, so we are expanding
+                                and improve the quality and range of our services. In 2008
+                                our company at the ceremony "Annual Presidential Achievement Award
+                                in the field of foreign economic relations and trade "was awarded an award from
+                                Ministry of Economic Education for achieving a turnover of more than $ 30 million.",
+                                
+                                about_sub_text2: "In 2015, for more convenient transportation and sale of products, our agency was opened in Primorye.
+                                We supply local produce wholesale and retail, and we have refrigerated and freezer systems for sale
+                                with chilled and frozen food.",
+
+                                time1: "1993",
+                                timetitle1: "Founding",
+                                timetitle1_text: "Founding of Katyusha Trading",
+                                time2: "Nov 8, 1996",
+                                timetitle2: "Presidential award",
+                                timetitle2_text: "Presidential award for achievements in the field of foreign economic relations and trade(trade turnover exceeded $ 10 million)",
+                                time3: "Dec 1, 1999",
+                                timetitle3: "Branch in Russia",
+                                timetitle3_text: "Opening of a branch in Russia (Vladivostok)",
+                                time4: "Apr 1, 2000",
+                                timetitle4: "Reorganization",
+                                timetitle4_text: "Reorganization of the company into the Upkite Corporation",
+                                time5: "2001",
+                                timetitle5: "Branch in Russia",
+                                timetitle5_text: "Opening of a branch in Novosibirsk",
+                                time6: "Dec, 2002",
+                                timetitle6: "Legal organization in Russia",
+                                timetitle6_text: "Opening of an independent legal organization in Vladivostok",
+                                time7: "2002",
+                                timetitle7: "Presidential award",
+                                timetitle7_text: "Presidential award for achievements in the field of foreign economic relations and trade",
+                                time8: "2004",
+                                timetitle8: "Branch in Kazakhstan and Kyrgyzstan",
+                                timetitle8_text: "Opening of a branch in Kazakhstan and Kyrgyzstan",
+                                time9: "2006",
+                                timetitle9: "Presidential award",
+                                timetitle9_text: "Presidential award for achievements in the field of foreign economic relations and trade(trade turnover exceeded $ 20 million)",
+                                time10: "Nov 30, 2008",
+                                timetitle10: "Presidential award",
+                                timetitle10_text: "Presidential award for achievements in the field of foreign economic relations and trade(trade turnover exceeded $ 30 million). Award from the Ministry of Economic Education.",
+                                time11: "Dec, 2009",
+                                timetitle11: "Reorganization",
+                                timetitle11_text: "Recognized as a professional trading company by the Chamber of Commerce and Industry of the Republic Korea",
+                                time12: "Dec, 2010",
+                                timetitle12: "Over $43 million turnover",
+                                timetitle12_text: "The company's annual turnover exceeded $ 43 million.",
+                                time13: "Nov, 2011",
+                                timetitle13: "Prime Minister's award",
+                                timetitle13_text: "Award of the Prime Minister of the Republic of Korea on Foreign Trade Day.",
+                                time14: "Apr, 2015",
+                                timetitle14: "Branch in Russia",
+                                timetitle14_text: "Opening a branch in Russia (Vladivostok).",
+                                busan_address: "Head office in Busan Address: 6F, 226, Asiad-daero, Dongnae-gu, Busan, Republic of Korea(47837)</br>Tel .: 051-557-6781 Fax: 051-556-0084 e-mail: izaccola@daum.net",
+                                russian_address: "Office and warehouse in Vladivostok. Address: Vladivostok, st. Aleutskaya 51, 39. </br> Tel : + 7-908-961-3591. e-mail: lyuda_kim73@mail.ru",
+
+                                category: "Category",
+                                weight: "Capacity",
+                                company: "Manifacture",
+                                expiration: "Expiration date"
+
+                                recipes_head_title: "Korean Cuisine Recipes",
+                                recipes_head_text: "Learn how to cook with Upkait",
+
+                                recipe1_title: "DUMPLING SOUP",
+                                recipe1_title: "1. Separate egg whites and yolks, fry finely and chop.<br>
+                                2. Pour 500 ml of water into a saucepan and boil, boil half of the beef sticks again, add the
+                                dumplings and chopped potatoes.<br>
+                                3. When the dumplings and chopped potatoes are done, add the green onions, garlic and peppers.<br>
+                                4. Put it in a bowl and add the egg to it.",
+                            
+                                recipe2_title: "CURRY",
+                                recipe2_text: "<strong>Curry Rice (Carrie Rice / Kare Rice)</strong> <br>
+                                <strong>Ingredients:</strong> Meat 150 g (fillet of pork, beef, turkey or
+                                chicken) or seafood, seasoning Ottogi curry 100 g 1 pc, onions 1-2 pcs., Potatoes 3 pcs, carrots 1
+                                pc, vegetable oil 30 g, 700 ml of water, salt, pepper taste. <br>
+                                <br>
+                                Preparation: Photo-recipe for curry
+                                rice (curry rice / kare rice) Cut meat, onions, carrots, potatoes into half a centimeter cubes. Fry
+                                the meat in vegetable oil in a deep frying pan, add the chopped vegetables and continue frying. Add
+                                500 ml of water to the meat with vegetables and simmer over low heat until almost cooked. Stir
+                                occasionally. Stir 100 g curry powder in 200 ml cold water and pour into stewed vegetables and meat,
+                                mix thoroughly and simmer until cooked. Place hot rice on a plate and top with curry rice. Bon
+                                Appetit!",
+                            
+                                recipe3_title: "TTEOKBOKKI",
+                                recipe3_text: "<strong>Noodle Tteok-bokki Recipe</strong> (for 2 servings) <br>
+                                1. Rinse 200g (2 bags) of rice cakes with water and remove. <br>
+                                2. Put 400ml (2 cups) of water in a pot
+                                and boil. <br>
+                                3. When the water starts to boil, add 1 ramen noodles, 120g (2 bags) of rinsed rice cakes
+                                and sauce, 6g (2 bags) of dried busan fish cake and boil for 4-5 minutes to complete.<br>
+                                *It is delicious even if you enjoy boiled eggs, tempura, various kinds of fish,
+                                and vegetables to suit your taste.",
+                            
+                                recipe33_text: "<strong>Soup Tteok-bokki Recipe</strong> (for 2 servings)<br>
+                                1. Rinse 300g (2 bags) of rice cakes with water and remove. <br>
+                                2. Put 300ml (1 and 1/2 cups) of water
+                                in a pan, and boil it with the rinsed rice cake and sauce 120g (2 bags) and vegetable garnish 4g (2
+                                bags).<br>
+                                3. Stir well for 5~6 minutes until the mochi is soft and finished.<br>
+                                *It is delicious even if you add fish cake, boiled egg, ramen etc. to suit your taste.",
+
+                                
+
+                                contact_head_title: "We will do our best to achieve 100% customer satisfaction.",
+                                contact_head_text: "If you submit an inquiry with your full contact details, we will respond to you as soon as possible following confirmation by the person in charge of the relevant department.",
+                                contact_busan: "Head office in Busan Address:",
+                                contact_busan_address: "6F, 226, Asiad-daero, Dongnae-gu, Busan, Republic of Korea(47837) </br>
+                                Tel .: 051-557-6781 </br>
+                                Fax: 051-556-0084 </br>
+                                e-mail: izaccola@daum.net",
+                                contact_russia: "Office and warehouse in Vladivostok",
+                                contact_russian_address: "Address: Vladivostok, st. Aleutskaya 51, 39. </br>
+                                Tel : + 7-908-961-3591. </br>
+                                e-mail: lyuda_kim73@mail.ru", 
+                                form_title: "GET IN TOUCH",
+                                name: "Name",
+                                email: "Email",
+                                company: "Company name",
+                                phone: "Contact number",
+                                message: "Your message",
+                                send: "Send"
+
+                                }
